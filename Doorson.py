1a73c983a5f47aa21c1806a32156fff1ef5804d4
RitoruX <ppittayut.ritoru@gmail.com>
2021-02-18 14:07:50 +0700

    fix missing main.

--- Doorson.py
+++ Doorson.py
@@ -24,6 +24,8 @@ def check_in():
     doorsonCollections.insert(check_in_query)
     return {"result" : "Checkin Successfully"}
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='3000', debug=True)
 # @app.route("/check_out", methods=["DELETE"])
 # def checkout():
 #     data = request.json()
