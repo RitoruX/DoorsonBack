82a7528533034c4f56692c504f518ddf769b1528
RitoruX <ppittayut.ritoru@gmail.com>
2021-02-18 14:19:04 +0700

    fix timezone in timestamp..

--- Doorson.py
+++ Doorson.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
-from datetime import datetime
-import pytz
+from datetime import datetime, pytz
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
