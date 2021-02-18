7ff4ab6de2a0bd51a19f2c015473d0d9a3d0c769
RitoruX <ppittayut.ritoru@gmail.com>
2021-02-18 16:44:48 +0700

    fix show_n method.

--- Doorson.py
+++ Doorson.py
@@ -35,7 +35,7 @@ def show_n():
                     "total_users" : {"$sum" : "$n_persons"}
                 } 
             }
-        ]).toArray()
+        ]).findall()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='3000', debug=True)
